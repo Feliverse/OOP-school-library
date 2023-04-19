26d653369303225258edc545ee6c866f05d10354
Feliverse <haybar@gmail.com>
2023-04-19 19:35:35 -0400

    fix rubocop offenses

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative './decorator'
 
 # correct a name to capitalized
--- decorator.rb
+++ decorator.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative './nameable'
 
 # Correct a name to capitalized and trimmer
