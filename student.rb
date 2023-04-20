a75f4419b8b03650fb20174a047c18b20c0aefb1
Feliverse <haybar@gmail.com>
2023-04-20 10:56:16 -0400

    create ralation between Classroom and Student

--- student.rb
+++ student.rb
@@ -2,6 +2,8 @@ require_relative './person'
 
 # to crate a student
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -9,5 +11,10 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.include?(self)
   end
 end
