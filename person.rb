b75bb4438ecbf94f9912f45b641b72afaf3c8d66
Feliverse <haybar@gmail.com>
2023-04-20 15:41:57 -0400

    Relation Person and Book intermediate Rental

--- person.rb
+++ person.rb
@@ -2,15 +2,16 @@ require_relative './nameable'
 
 # create a person
 class Person < Nameable
+  attr_accessor :name, :age, :rentals
   attr_reader :id
-  attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    super
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   private
@@ -27,5 +28,9 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(book, date)
+    Rental.new(self, book, date)
   end
 end
