b52588e2c3b72579961933e80cd71c4db74d12c8
Feliverse <haybar@gmail.com>
2023-04-25 16:27:38 -0400

    Refactor Read and update People

--- modules/people.rb
+++ modules/people.rb
@@ -1,5 +1,7 @@
 require_relative '../student'
 require_relative '../teacher'
+require_relative 'crud_people' 
+
 module PeopleListing
   class PeopleLister
     def list(peoples)
@@ -32,6 +34,7 @@ module PeopleListing
   end
 
   class StudentCreator
+    include CrudPeople
     def create(person)
       puts "\n"
       print 'Age: '
@@ -43,26 +46,27 @@ module PeopleListing
       print 'Classroom: '
       classroom = gets.chomp
 
-      parent_permission = permission == 'Y'
+      parentpermission = permission == 'Y'
 
-      student = Student.new(classroom, age, name, parent_permission: parent_permission)
-      person.push(student)
+      student = Student.new(classroom, age, name, parentpermission: parentpermission)
+      SavePeople.new.save([student])
+      #person.push(student)
       puts "\n Student created successfully!!"
     end
   end
 
   class TeacherCreator
+    include CrudPeople
     def create(person)
       print 'Age: '
       age = gets.chomp
       print 'Name: '
       name = gets.chomp
       print 'Specialization: '
-      specialization = gets.chomp
-
+      specialization = gets.chomp     
       teacher = Teacher.new(age, specialization, name)
-
-      person.push(teacher)
+      SavePeople.new.save([teacher])
+      #person.push(teacher)
 
       puts 'Teacher created successfully!!'
     end
