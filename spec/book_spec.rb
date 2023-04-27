2aabe10a4ea300353b5ea39ebb62488f01a79263
anderCM <andercm15@gmail.com>
2023-04-27 01:32:39 -0500

    Tests description renamed

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,13 +5,14 @@ require_relative '../student'
 
 describe Book do
   context 'Testing Book class and method' do
-    it 'Creates a book' do
+    it 'Should return the recent author and title' do
       book = Book.new('Game of thrones', 'George R. Martin')
       message = "#{book.author} wrote #{book.title}"
+      
       expect(message).to eq 'George R. Martin wrote Game of thrones'
     end
 
-    it 'Add a rental using add_rental method' do
+    it 'Should validate if created rental is stored in book' do
       book = Book.new('The ring', 'Idk')
       student = Student.new('c-808', 22, 'Anderson')
       date = Date.today
